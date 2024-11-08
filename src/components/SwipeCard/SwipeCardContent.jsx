b8acb2603d23d87e152fb2372eef942d699678d0
papumaja <papumaja@protonmail.com>
2024-11-09 00:11:55 +0200

    Paskaaha se on mut tulipaha tehty

--- src/components/SwipeCard/SwipeCardContent.jsx
+++ src/components/SwipeCard/SwipeCardContent.jsx
@@ -5,6 +5,7 @@ import { CardsContext } from '../../context/CardsContext';
 import '../../index.css'
 import './SwipeCardContent.css'
 
+
 function LikeButton()
 {
     return <button>btn</button>
@@ -19,10 +20,45 @@ function DislikeButton()
     return <button>btn</button>
 }
 
+function SwipeCardPreview() {
+    // Consume the CardsContext
+    const { cards, currentIndex, swipeCard } = useContext(CardsContext);
+    const currentCard = cards[currentIndex];
+    return (
+        <div>
+            <Typography
+            variant="body2"
+            color="textSecondary"
+            style={{ textAlign: 'center', marginTop: '8px' }}
+            >
+            {currentCard.description}
+            </Typography>
+        </div>
+    );
+}
+
+function SwipeCardImage()
+{
+    // Consume the CardsContext
+    const { cards, currentIndex, swipeCard } = useContext(CardsContext);
+    const currentCard = cards[currentIndex];
+    return <div className="SwipeCardImage">
+        {currentCard.image && (
+            <CardMedia
+            component="img"
+            image={currentCard.image}
+            alt={currentCard.name}
+            style={{ height: '60%', objectFit: 'cover' }}
+            />
+        )}
+    </div>
+}
+
 export function SwipeCardContent()
 {
     // Consume the CardsContext
     const { cards, currentIndex, swipeCard } = useContext(CardsContext);
+    const currentCard = cards[currentIndex];
 
     // Check if there are any cards left
     if (currentIndex >= cards.length) {
@@ -33,38 +69,21 @@ export function SwipeCardContent()
         );
     }
 
-    const currentCard = cards[currentIndex];
     return (
         <div>
-            <div className="SwipeCardTitle">Test Card</div>
-            <div className="SwipeCardImage">
-                {currentCard.image && (
-                    <CardMedia
-                    component="img"
-                    image={currentCard.image}
-                    alt={currentCard.name}
-                    style={{ height: '60%', objectFit: 'cover' }}
-                    />
-                )}
-            </div>
-            <div className="SwipeCardCompatibility"></div>
-            <div className="SwipeCardPreview">
+            <div className="SwipeCardTitle">
                 <Typography variant="h5" component="div" style={{ textAlign: 'center' }}>
                 {currentCard.name}
                 </Typography>
-                <Typography
-                variant="body2"
-                color="textSecondary"
-                style={{ textAlign: 'center', marginTop: '8px' }}
-                >
-                {currentCard.description}
-                </Typography>
             </div>
+            <div className="SwipeCardCompatibility"></div>
+            <SwipeCardPreview className="SwipeCardPreview"/>
             <div className="SwipeCardControls">
                 <DislikeButton></DislikeButton>
                 <DetailsButton></DetailsButton>
                 <LikeButton></LikeButton>
             </div>
+            <SwipeCardImage className="SwipeCardImage"/>
         </div>
     );
 }
